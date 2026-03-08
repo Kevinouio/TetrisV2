9f3c26a69bb668572d8151c61775f86310a7de5c
Kevin Ho <kevintho2005@gmail.com>
2026-03-08 16:45:58 -0500

    Clearing some files

--- TetrisVersionTwo/src/agent.cpp
+++
@@ -1,3 +0,0 @@
-#include "tetris_v2/agent.hpp"
-
-// Intentionally empty translation unit; interfaces are defined inline in headers.
--- TetrisVersionTwo/src/batch_env.cpp
+++
@@ -1,3 +0,0 @@
-#include "tetris_v2/tetris_env.hpp"
-
-// Placeholder for a future vectorized environment implementation.
